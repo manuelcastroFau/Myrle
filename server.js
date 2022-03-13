49e05873b09341c0cf3d620a2b23b8561d1a223b
Manuel Castro <manuelcastro2021@fau.edu>
2022-03-13 18:35:39 -0400

    First Version of MainMenu page

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-//app.use(bodyParser.urleconded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('Assets'));
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname+'/index.html');
@@ -14,6 +16,13 @@ app.get('/', (req,res) => {
 app.get("/register", (req, res) =>{
   res.sendFile(__dirname+'/register.html');
   //res.send("sss");
+
+
+});
+
+app.get("/menu",(req,res) =>{
+
+  res.sendFile(__dirname+"/menu-section.html");
 });
 
 app.listen(3000, function() {
