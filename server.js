856100606cdcf7b1bd24b1d37c1a65c4019ef654
Manuel Castro <manuelcastro2021@fau.edu>
2022-03-14 01:09:01 -0400

    Event page created and all  Launching Soon Page created, basic routing working

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('Assets'));
+app.use(express.static('Assets/js'));
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname+'/index.html');
@@ -24,6 +25,37 @@ app.get("/menu",(req,res) =>{
 
   res.sendFile(__dirname+"/menu-section.html");
 });
+
+app.get("/events",(req,res) =>{
+  
+  res.sendFile(__dirname+"/events.html");
+});
+
+app.get("/profile",(req,res) =>{
+  
+  res.sendFile(__dirname+"/profile.html");
+});
+
+app.get("/social-life",(req,res) =>{
+  
+  res.sendFile(__dirname+"/social-life.html");
+});
+
+app.get("/information",(req,res) =>{
+  
+  res.sendFile(__dirname+"/information.html");
+});
+
+app.get("/billing",(req,res) =>{
+  
+  res.sendFile(__dirname+"/billing.html");
+});
+app.get("/community",(req,res) =>{
+  
+  res.sendFile(__dirname+"/community.html");
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started in port 3000");
